273f274b96a899c585297a96f9b9164ffa8a4fd5
deveHk <connect.to.hk@gmail.com>
2023-05-07 02:34:43 +0530

    fix: faq content

--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -4,6 +4,59 @@ import Dropdown from "./useDropdown";
 const bungee = Bungee_Inline({ subsets: ["latin"], weight: "400" });
 
 export default function Faq() {
+  const faq = [
+    [
+      "This section is not available right now..",
+      "This section is not available right now..",
+    ],
+    [
+      "This section is not available right now..",
+      "This section is not available right now..",
+    ],
+    [
+      "This section is not available right now..",
+      "This section is not available right now..",
+    ],
+    [
+      "This section is not available right now..",
+      "This section is not available right now..",
+    ],
+    [
+      "This section is not available right now..",
+      "This section is not available right now..",
+    ],
+    [
+      "This section is not available right now..",
+      "This section is not available right now..",
+    ],
+  ];
+  const faqq = [
+    [
+      "How much does it cost to participate?",
+      "Nothing, participation in HackHound is absolutely FREE!",
+    ],
+    [
+      "What is the venue for the event?",
+      "The offline hackathon will be held in SRM Institute of Science and Technology, Modinagar, Ghaziabad. Other details will be shared with you via email.",
+    ],
+    [
+      "Who can participate? Do any prerequisites or skills required to exist?",
+      "Participation in this event is open to everyone. The only prerequisites are excitement and motivation. It’s a beginner-friendly hackathon. We are encouraging beginners to begin their coding journey with us. But having coding and programming experience is a significant benefit.",
+    ],
+
+    [
+      "What are the rules on the Team Size?",
+      "Participation in this event is open to everyone. The only prerequisites are excitement and motivation. It’s a beginner-friendly hackathon. We are encouraging beginners to begin their coding journey with us. But having coding and programming experience is a significant benefit.",
+    ],
+    [
+      "Will there be food?",
+      "Yes, you will be provided with meals. We don’t want to keep our hackers on empty stomachs. And we know the importance of caffeine to keep on with the code hiking so a lot of coffee will also be available for our hackers.",
+    ],
+    [
+      "Are you required to be enrolled in a degree program?",
+      "Yes, you are required to be enrolled in degree program in order to participate in the hackathon as student.",
+    ],
+  ];
   return (
     <div className="max-w-7xl m-auto pt-20" id="faq">
       <h1
@@ -11,22 +64,9 @@ export default function Faq() {
       >
         FAQs
       </h1>
-      <div className="faq-container">
-        <div className="col">
-          {[
-            [
-              "How much does it cost to participate?",
-              "Nothing, participation in HackHound is absolutely FREE!",
-            ],
-            [
-              "What is the venue for the event?",
-              "The offline hackathon will be held in SRM Institute of Science and Technology, Modinagar, Ghaziabad. Other details will be shared with you via email.",
-            ],
-            [
-              "Who can participate? Do any prerequisites or skills required to exist?",
-              "Participation in this event is open to everyone. The only prerequisites are excitement and motivation. It’s a beginner-friendly hackathon. We are encouraging beginners to begin their coding journey with us. But having coding and programming experience is a significant benefit.",
-            ],
-          ].map((e) => {
+      <div className="grid md:grid-cols-2 w-[80%] md:w-[90%] md:gap-[10%] ">
+        <div className=" w-full space-y-4">
+          {faq.slice(0, 3).map((e) => {
             return (
               <div className="row" key={e[0]}>
                 <Dropdown title={e[0]} description={e[1]}></Dropdown>
@@ -34,21 +74,8 @@ export default function Faq() {
             );
           })}
         </div>
-        <div className="col">
-          {[
-            [
-              "What are the rules on the Team Size?",
-              "Participation in this event is open to everyone. The only prerequisites are excitement and motivation. It’s a beginner-friendly hackathon. We are encouraging beginners to begin their coding journey with us. But having coding and programming experience is a significant benefit.",
-            ],
-            [
-              "Will there be food?",
-              "Yes, you will be provided with meals. We don’t want to keep our hackers on empty stomachs. And we know the importance of caffeine to keep on with the code hiking so a lot of coffee will also be available for our hackers.",
-            ],
-            [
-              "Are you required to be enrolled in a degree program?",
-              "Yes, you are required to be enrolled in degree program in order to participate in the hackathon as student.",
-            ],
-          ].map((e) => {
+        <div className="space-y-4 w-full mt-4 md:mt-0">
+          {faq.slice(3).map((e) => {
             return (
               <div className="row" key={e[0]}>
                 <Dropdown title={e[0]} description={e[1]}></Dropdown>
