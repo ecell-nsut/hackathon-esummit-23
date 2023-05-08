1da35a23f50ab2526651c94ecb5a1c8a17fae3ea
Arnav Gupta <info@arnavgupta.net>
2023-05-08 14:51:37 +0530

    feature: Animation Added again

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -20,13 +20,14 @@ const Layout = ({ children }: any) => {
 
   return (
     <>
-      <main
-        className="bg-[#0E0E0E] bg-opacity-10 min-h-screen max-w-screen overflow-hidden"
-        id="main-screen"
-        style={{ width: "100%", margin: 0 }}
-      >
+      <main className="bg-[#0E0E0E] bg-opacity-10 min-h-screen max-w-screen overflow-hidden">
         <div className="min-h-screen fixed  bg-black -z-[1] top-0 w-screen">
           <div className="absolute top-0 z-[1] backdrop-blur-[12vmax] h-full w-full bg-[#231F20] bg-opacity-10"></div>
+          <div
+            ref={blobRef}
+            className={`origin-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 blob aspect-square bg-gradient-to-r from-blue-600  via-purple-800 to-black  h-[40vmax]
+            }] rounded-full bg-pink-300 opacity-70 animate-rotate`}
+          ></div>
         </div>
         <Navbar />
         {children}
